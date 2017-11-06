25c43574522ec16cd21b66d1708adb960c2e099c
Pandre <pandre.barbosa@gmail.com>
2017-11-05 22:45:53 -0200

    Resetar senha

---
+++ condominio/services/usuarios/resetarSenha.php
@@ -0,0 +1,28 @@
+<?php
+require_once (realpath(dirname(dirname(dirname(__FILE__)))) . '/services/lib/inicializa.php');
+
+include(realpath(dirname(dirname(dirname(__FILE__)))) . '/entidades/Usuario.class.php');
+include(realpath(dirname(dirname(dirname(__FILE__)))) . '/DAO/UsuarioDAO.class.php');
+
+$Usuario = new Usuario($_POST);
+$Usuario->setCoPessoaRegistro($_SESSION['credencial']['co_pessoa']);
+
+$UsuarioDAO = new UsuarioDAO();
+
+$UsuarioDAO->resetarSenha($Usuario);
+
+$destinatario = $UsuarioDAO->listarUsuarioJSON($Usuario);
+
+$enviou = Email::emailResetarSenha($destinatario[0]['ds_email']);
+
+$resultado = "";
+if($enviou){
+    $resultado =  array("tipo" => "sucesso",
+        "msg" => "Senha alterada com sucesso. Usuário alertado por email.");
+}else{
+    $resultado =  array("tipo" => "sucesso",
+        "msg" => "Senha alterada com sucesso. Não houve envio de email por erro desconhecido.");
+}
+
+echo json_encode($resultado);
+exit;
